3ce212a3b644f5cb4e108dee1588eceec79dd494
Daniele <danielerb.dev@gmail.com>
2025-12-16 19:42:02 +0100

    Finish the 'add_entry' function

--- main.py
+++ main.py
@@ -22,30 +22,20 @@ def load_entries():
         except json.decoder.JSONDecodeError:
             entries = {}
     return entries
-    # print(f'Title: {entry["title"]}')
 
 
-def save_entries(entries):
+def save_entries(entry):
     """Save journal entries to the JSON file."""
     journal_entries = load_entries()
-    for entry in entries:
-        journal_entries.update(entry)
+    journal_entries.update({entry.title: entry.content})
     with open(DB_FILE, mode="w", encoding="utf-8") as write_file:
         json.dump(journal_entries, write_file)
 
 
 def add_entry(title, content):
     """Create a new journal entry and save it to the JSON file."""
-
-    # TODO
-    """
     new_journal_entry = JournalEntry(title, content)
-    entries = load_entries()
-    entries.update(vars(new_journal_entry))
-    print(entries)
-    save_entries(entries)
-    """
-    pass
+    save_entries(new_journal_entry)
 
 
 def list_entries(entries):
@@ -57,11 +47,9 @@ if __name__ == "__main__":
     # quick interactive program (entry point) to validate the above
     title = input("Title: ")
     content = input("Content: ")
-    # add_entry(title, content)
+    add_entry(title, content)
     print("✅ Entry saved.")
     print("\n📘 Your Journal:")
     # list_entries(load_entries())
 
-    # Hardcode values to test the 'save_entries' and 'load_entries' functions
-    save_entries([{"c": "d"}, {"e": "f"}])
     print(load_entries())
